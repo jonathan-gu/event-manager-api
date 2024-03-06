ae78d726668ac799d6009de293a39fc0cfb5d05c
jonathan <jonathanguerpillon@gmail.com>
2024-03-06 14:24:15 +0100

    first commit

---
+++ event-manager-api/src/main/java/com/jg/eventmanagerapi/EventManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.jg.eventmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventManagerApiApplication.class, args);
+	}
+
+}
---
+++ event-manager-api/src/test/java/com/jg/eventmanagerapi/EventManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jg.eventmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
